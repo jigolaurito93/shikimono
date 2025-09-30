351fcebdba182beebd084c847b3b87a444abba5d
jigolaurito93 <jigolaurito93@yahoo.com>
2025-09-30 16:09:00 -0400

    added footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react";
+import { FaFacebook, FaInstagram, FaTiktok } from "react-icons/fa";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <div className="flex w-full px-80 py-20 bg-black text-white">
+      <div className="w-1/3  text-xl flex flex-col gap-2 font-medium">
+        <div>Track Order Status</div>
+        <div>Contact Us</div>
+        <div>Privacy Policy</div>
+        <div> Refund Policy</div>
+        <div>Terms of Service</div>
+      </div>
+      <div className="w-1/3 flex justify-center items-center gap-8 text-4xl">
+        <FaFacebook />
+        <FaInstagram />
+        <FaTiktok />
+      </div>
+      <div className="w-1/3 ">s</div>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
