254a9b0ed38a2758e28c23e5eacaef4c88988745
jigolaurito93 <jigolaurito93@yahoo.com>
2025-09-30 08:20:18 -0400

    ran es lint

---
+++ app/components/Hero.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import Image from "next/image";
+
+const Hero = () => {
+  return (
+    <div className="flex bg-[#6dc4b7] text-white">
+      {/* Left side image */}
+      <div className="w-xl">
+        <Image
+          src="/claytonshot.png"
+          alt="hero"
+          width={1000}
+          height={1000}
+          className="object-cover"
+        />
+      </div>
+
+      {/* Right side content */}
+      <div className="flex-1 flex flex-col justify-center items-center gap-4 px-36">
+        <h1 className="text-8xl font-bold font-mochiy-pop">Shikimono</h1>
+        <p className="text-center text-3xl px-16 my-8 font-medium font-m-plus-1">
+          We weave luxury into every fiber of our soft velvety mats,
+          transforming spaces with elegance and comfort.
+        </p>
+        <button className="px-6 py-3 bg-black cursor-pointer text-white rounded-lg hover:bg-gray-800 transition">
+          Shop Now
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
---
+++ app/components/Navbar.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import Image from "next/image";
+import { FaCartArrowDown } from "react-icons/fa6";
+import { CiUser } from "react-icons/ci";
+import { CiSearch } from "react-icons/ci";
+
+const Navbar = () => {
+  return (
+    <div className="flex w-full h-full py-4 px-20 items-center">
+      <div className="w-5xl">
+        <Image src="/swathelogo.png" alt="logo" width={200} height={100} />
+      </div>
+      <ul className="flex gap-8 items-center font-medium text-lg">
+        <li className="cursor-pointer underline">Home</li>
+        <li className="cursor-pointer">Shop</li>
+        <li className="cursor-pointer">About</li>
+        <li className="cursor-pointer">FAQs</li>
+        <li className="cursor-pointer">Contact</li>
+        <div className="flex gap-4 text-2xl">
+          <FaCartArrowDown />
+          <CiUser />
+          <CiSearch />
+        </div>
+      </ul>
+    </div>
+  );
+};
+
+export default Navbar;
