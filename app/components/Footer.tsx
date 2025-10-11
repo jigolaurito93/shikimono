567bd0570b96caf006af88ae712483b2a6c081ca
jigolaurito93 <jigolaurito93@yahoo.com>
2025-10-11 08:58:52 -0400

    fixed spacing  and padding

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -3,20 +3,20 @@ import { FaFacebook, FaInstagram, FaTiktok } from "react-icons/fa";
 
 const Footer = () => {
   return (
-    <div className="flex flex-col md:flex-row w-full px-8 md:px-16 lg:px-24 py-12 lg:py-14 bg-black text-white gap-8">
-      <div className="w-full lg:w-1/3 text-xl flex flex-col gap-4 lg:gap-2 font-medium">
+    <div className="flex flex-col md:flex-row w-full px-8 md:px-16 lg:px-24 py-12 lg:py-14 bg-black text-white gap-12 xl:px-56">
+      <div className="w-full lg:w-1/3 text-md flex flex-col gap-4 lg:gap-2 font-medium">
         <div>Track Order Status</div>
         <div>Contact Us</div>
         <div>Privacy Policy</div>
         <div> Refund Policy</div>
         <div>Terms of Service</div>
       </div>
-      <div className="w-1/2 md:w-full lg:w-1/3 flex justify-center items-center gap-8 text-4xl lg:justify-start">
+      <div className="md:w-full text-2xl md:text-4xl lg:w-1/3 flex justify-center md:justify-start items-center gap-8 lg:justify-start">
         <FaFacebook />
         <FaInstagram />
         <FaTiktok />
       </div>
-      <div className="hidden lg:flex w-1/3 text-xl flex-col gap-2 font-medium">
+      <div className="hidden lg:flex w-1/3 text-md flex-col gap-2 font-medium">
         <div>Track Order Status</div>
         <div>Contact Us</div>
         <div>Privacy Policy</div>
