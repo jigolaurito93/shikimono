03ab4eeed89f949cdf8791c470dbf2636bc92632
Jose Laurito <jigolaurito93@yahoo.com>
2025-10-11 06:05:21 -0400

    Finished footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -3,20 +3,26 @@ import { FaFacebook, FaInstagram, FaTiktok } from "react-icons/fa";
 
 const Footer = () => {
   return (
-    <div className="flex w-full px-80 py-20 bg-black text-white">
-      <div className="w-1/3  text-xl flex flex-col gap-2 font-medium">
+    <div className="flex flex-col md:flex-row w-full px-8 md:px-16 lg:px-24 py-12 lg:py-14 bg-black text-white gap-8">
+      <div className="w-full lg:w-1/3 text-xl flex flex-col gap-4 lg:gap-2 font-medium">
         <div>Track Order Status</div>
         <div>Contact Us</div>
         <div>Privacy Policy</div>
         <div> Refund Policy</div>
         <div>Terms of Service</div>
       </div>
-      <div className="w-1/3 flex justify-center items-center gap-8 text-4xl">
+      <div className="w-1/2 md:w-full lg:w-1/3 flex justify-center items-center gap-8 text-4xl lg:justify-start">
         <FaFacebook />
         <FaInstagram />
         <FaTiktok />
       </div>
-      <div className="w-1/3 ">s</div>
+      <div className="hidden lg:flex w-1/3 text-xl flex-col gap-2 font-medium">
+        <div>Track Order Status</div>
+        <div>Contact Us</div>
+        <div>Privacy Policy</div>
+        <div> Refund Policy</div>
+        <div>Terms of Service</div>
+      </div>
     </div>
   );
 };
